787e529b6127950adc1896c2497e05c905eefed7
ardyikhsanudin <muh.ardyikhsanudin@gmail.com>
2023-01-02 09:52:02 +0700

    update bootcamp

--- echo_display_variable.php
+++ echo_display_variable.php
@@ -12,6 +12,16 @@
     echo "Study PHP at " . $txt2 . "<br>";
     echo $x + $y;
     ?>
+
+<?php
+    $txt1 = "Learn PHP";
+    $txt2 = "Regarsport";
+    $x = 5;
+    $y = 4;
+    echo "<h2>" . $txt1 . "</h2>";
+    echo "Study PHP at " . $txt2 . "<br>";
+    echo $x + $y;
+    ?> 
 </body>
  
 </html>
